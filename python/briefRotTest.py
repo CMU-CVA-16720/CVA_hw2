dca81cb3217535600710c53da632ab707997bd82
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-09-25 08:54:02 -0400

    Initial commit

---
+++ python/briefRotTest.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+from matchPics import matchPics
+from opts import get_opts
+
+opts = get_opts()
+#Q2.1.6
+#Read the image and convert to grayscale, if necessary
+
+for i in range(36):
+	#Rotate Image
+	
+	#Compute features, descriptors and Match features
+
+	#Update histogram
+
+	pass # comment out when code is ready
+
+
+#Display histogram
+
