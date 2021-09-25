d62c7a8b1970a6681b28369291c169f86527150b
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-09-25 11:23:07 -0400

    Added convert to grayscale

--- python/matchPics.py
+++ python/matchPics.py
@@ -6,22 +6,24 @@ from helper import computeBrief
 from helper import corner_detection
 
 def matchPics(I1, I2, opts):
-	#I1, I2 : Images to match
-	#opts: input opts
-	ratio = opts.ratio  #'ratio for BRIEF feature descriptor'
-	sigma = opts.sigma  #'threshold for corner detection using FAST feature detector'
-	
+    #I1, I2 : Images to match
+    #opts: input opts
+    ratio = opts.ratio  #'ratio for BRIEF feature descriptor'
+    sigma = opts.sigma  #'threshold for corner detection using FAST feature detector'
 
-	#Convert Images to GrayScale
-	
-	
-	#Detect Features in Both Images
-	
-	
-	#Obtain descriptors for the computed feature locations
-	
+    #Convert Images to GrayScale
+    togray = lambda img: 0.2989*img[:,:,0] + 0.5870*img[:,:,1] + 0.1140*img[:,:,2]
+    I1g = togray(I1)
+    I2g = togray(I2)
+    
+    
+    #Detect Features in Both Images
+    
+    
+    #Obtain descriptors for the computed feature locations
+    
 
-	#Match features using the descriptors
-	
+    #Match features using the descriptors
+    
 
-	return matches, locs1, locs2
+    return matches, locs1, locs2
