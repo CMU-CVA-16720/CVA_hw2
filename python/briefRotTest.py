Implemented rotation and histogram. Currently uses bar graph. Image is 1/10th correct size for speed.

python/briefRotTest.py
@@ -2,20 +2,35 @@ import numpy as np
 import cv2
 from matchPics import matchPics
 from opts import get_opts
+from scipy.ndimage import rotate
+import matplotlib.pyplot as plt
+#from helper import plotMatches
 
 opts = get_opts()
 #Q2.1.6
 #Read the image and convert to grayscale, if necessary
-
+original = cv2.imread('../data/cv_cover.jpg')
+original = original[::10,::10,:]
+results = np.zeros([36])
+lbls = []
 for i in range(36):
-	#Rotate Image
-	
-	#Compute features, descriptors and Match features
-
-	#Update histogram
-
-	pass # comment out when code is ready
+    #Rotate Image
+    rotated = rotate(original,i*10)
+#    plt.imshow(rotated)
+#    plt.show()
+    #Compute features, descriptors and Match features
+    matches, locs1, locs2 = matchPics(original, rotated, opts)
+    #Update histogram
+    results[i] = matches.shape[0]
+    lbls.append(10*i)
 
 
 #Display histogram
+results = results.astype('int')
+print("Results:\n{}".format(results))
+plt.bar(lbls,results,width=9.8)
+plt.xlabel('Rotation (deg)')
+plt.ylabel('Matches')
+plt.title('Matches vs Rotation')
+plt.show()
 