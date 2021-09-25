6127a23be7e300624022d08ef3be4cf87156e865
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-09-25 11:40:49 -0400

    Implemented matchPics

--- python/matchPics.py
+++ python/matchPics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 import skimage.color
-from helper import briefMatch
+from helper import briefMatch, plotMatches
 from helper import computeBrief
 from helper import corner_detection
 
@@ -16,14 +16,15 @@ def matchPics(I1, I2, opts):
     I1g = togray(I1)
     I2g = togray(I2)
     
-    
     #Detect Features in Both Images
-    
+    locs1 = corner_detection(I1g,sigma)
+    locs2 = corner_detection(I2g,sigma)
     
     #Obtain descriptors for the computed feature locations
-    
+    desc1, locs1 = computeBrief(I1g,locs1)
+    desc2, locs2 = computeBrief(I2g,locs2)
 
     #Match features using the descriptors
-    
+    matches = briefMatch(desc1, desc2, ratio)
 
     return matches, locs1, locs2
