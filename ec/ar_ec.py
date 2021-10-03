9fa9cb3ad57dc6bc5f188d38f60dd0620bfebe17
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-10-03 13:36:50 -0400

    Cleaned up implementation and comments

--- ec/ar_ec.py
+++ ec/ar_ec.py
@@ -12,25 +12,17 @@ from opts import get_opts
 
 def compositePano(H2to1, addition, base):
     
-    #Create a composite image after warping the addition image on top
-    #of the image using the homography
+    # Combine images together; greater intensity gets priority
 
-    #Note that the homography we compute is from the image to the addition;
-    #x_addition = H2to1*x_photo
-    #For warping the addition to the image, we need to invert it.
-
-    #Create mask of same size as addition
-    mask = np.ones(addition.shape)
-
-    #Warp mask by appropriate homography
-    mask_warp = cv2.warpPerspective(mask, np.linalg.inv(H2to1), (base.shape[1],base.shape[0]))
-
-    #Warp addition by appropriate homography
+    # Warp addition by appropriate homography
     addition_warp = cv2.warpPerspective(addition, np.linalg.inv(H2to1), (base.shape[1],base.shape[0]))
 
-    #Use mask replace image when intensity is greater
+    # Fuse images
+    togray = lambda img: 0.2989*img[:,:,0] + 0.5870*img[:,:,1] + 0.1140*img[:,:,2]
+    base_g = togray(base)
+    add_g = togray(addition_warp)
+    replace_coord = add_g>base_g
     composite_base = base
-    replace_coord = np.all(addition_warp>base,axis=2)
     composite_base[replace_coord,:] = addition_warp[replace_coord,:]
     
     return composite_base
@@ -50,16 +42,16 @@ cv_desk=temp
 # plt.show()
 
 # Match cv cover and cv desk
-matches, locs1, locs2 = matchPics.matchPics(cv_cover, cv_desk, opts)
-np.savez_compressed('default_match.npz',
-    matches=matches,
-    locs1=locs1,
-    locs2=locs2
-)
-# default_match = np.load('default_match.npz')
-# locs1=default_match['locs1']
-# locs2=default_match['locs2']
-# matches=default_match['matches']
+# matches, locs1, locs2 = matchPics.matchPics(cv_cover, cv_desk, opts)
+# np.savez_compressed('default_match.npz',
+#     matches=matches,
+#     locs1=locs1,
+#     locs2=locs2
+# )
+default_match = np.load('default_match.npz')
+locs1=default_match['locs1']
+locs2=default_match['locs2']
+matches=default_match['matches']
 # plotMatches(cv_cover, cv_desk, matches, locs1, locs2)
 
 # convert (row,col) to (x,y), scaling for new image
